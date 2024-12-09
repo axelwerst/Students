a2c673250f82b8462fcef3596d9ef891317eceb0
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-12-09 13:07:54 +0100

    first

---
+++ src/main/java/com/gmail/axelwerst/App.java
@@ -0,0 +1,13 @@
+package com.gmail.axelwerst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
